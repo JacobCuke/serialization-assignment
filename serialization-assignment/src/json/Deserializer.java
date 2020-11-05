c2c725acfe8c95ab2d25148290eee5d4db2f6d52
jacob.cuke <jacob.cuke@cs.ucalgary.ca>
2020-11-05 16:44:58 -0700

    Deserialize primitive fields

--- serialization-assignment/src/json/Deserializer.java
+++ serialization-assignment/src/json/Deserializer.java
@@ -14,8 +14,8 @@ import javax.json.*;
  */
 public class Deserializer {
 
-	public static Object deserializeObject(JsonObject json_object) throws Exception {
-		JsonArray objectList = json_object.getJsonArray("objects");
+	public static Object deserializeObject(JsonObject jsonObject) throws Exception {
+		JsonArray objectList = jsonObject.getJsonArray("objects");
 		Map<String, Object> objectTrackingMap = new HashMap<String, Object>();
 		createInstances(objectTrackingMap, objectList);
 		assignFieldValues(objectTrackingMap, objectList);
@@ -27,17 +27,59 @@ public class Deserializer {
 		for (int i = 0; i < objectList.size(); i++) {
 			JsonObject objectInfo = objectList.getJsonObject(i);
 			Class<?> objectClass = Class.forName(objectInfo.getString("class"));
+			
+			// Instatiate object
 			Constructor<?> constructor = objectClass.getDeclaredConstructor();
 			if (!Modifier.isPublic(constructor.getModifiers())) {
 				constructor.setAccessible(true);
 			}
 			Object objectInstance = constructor.newInstance();
+			
 			//Make object
 			objectTrackingMap.put(objectInfo.getString("id"), objectInstance);
 		}
 	}
 
 	private static void assignFieldValues(Map<String, Object> objectTrackingMap, JsonArray objectList) throws Exception {
+		
+		for (int i = 0; i < objectList.size(); i++) {
+			JsonObject objectInfo = objectList.getJsonObject(i);
+			Class<?> objectClass = Class.forName(objectInfo.getString("class"));
+			
+			Object objectInstance = objectTrackingMap.get(objectInfo.getString("id"));
+			
+			// Fields
+			JsonArray fieldList = objectInfo.getJsonArray("fields");
+			
+			// Loop through each field, setting each one
+			for (int j = 0; j < fieldList.size(); j++) {
+				
+				JsonObject fieldInfo = fieldList.getJsonObject(j);
+				Field field = objectClass.getDeclaredField(fieldInfo.getString("name"));
+				field.setAccessible(true);
+				
+				Class<?> fieldType = field.getType();
+				
+				
+				// TODO: Handle more primitive types
+				if (fieldType.equals(Integer.TYPE)) {
+					
+					field.set(objectInstance, Integer.parseInt(fieldInfo.getString("value")));
+					
+				} else if (fieldType.equals(Float.TYPE)) {
+					
+					field.set(objectInstance, Float.parseFloat(fieldInfo.getString("value")));
+					
+				} else if (fieldType.equals(Boolean.TYPE)) {
+					
+					field.set(objectInstance, Boolean.parseBoolean(fieldInfo.getString("value")));
+					
+				}
+				
+			}
+			
+		}
+		
 	}
 
 }
